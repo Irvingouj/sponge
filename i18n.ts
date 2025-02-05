5d966d298fa65c4262fdab636b986a275f6f8992
zhouyuqi <2280356057@qq.com>
2025-02-05 18:51:25 +0800

    'zyq04'

--- i18n.ts
+++ i18n.ts
@@ -1,3 +1,18 @@
+// import { notFound } from 'next/navigation';
+// import { getRequestConfig } from 'next-intl/server';
+
+// // Can be imported from a shared config
+// const locales = ['en', 'zh'];
+
+// export default getRequestConfig(async ({ locale }) => {
+//     // Validate that the incoming `locale` parameter is valid
+//     if (!locales.includes(locale as any)) notFound();
+
+//     return {
+//         messages: (await import(`./messages/${locale}.json`)).default
+//     };
+// });
+
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
@@ -5,8 +20,10 @@ import { getRequestConfig } from 'next-intl/server';
 const locales = ['en', 'zh'];
 
 export default getRequestConfig(async ({ locale }) => {
-    // Validate that the incoming `locale` parameter is valid
-    if (!locales.includes(locale as any)) notFound();
+    // 如果请求的locale无效或者为空，强制使用'en'
+    if (!locale || !locales.includes(locale as any)) {
+        locale = 'en'; // 强制设置为英文
+    }
 
     return {
         messages: (await import(`./messages/${locale}.json`)).default
